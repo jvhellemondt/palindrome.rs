c6349878eacc24496082e4e1e5ce2cb0b1336fd7
Jens van Hellemondt <jens@invictus.codes>
2024-09-18 22:20:11 +0200

    test(palindrome): it_should_be_able_to_tell_that_mom_is_a_palindrome

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn it_should_be_able_to_tell_that_mom_is_a_palindrome() {
+        assert_eq!(PalindromeChecker::is_a_palindrome("mom"), true);
+    }
 }
